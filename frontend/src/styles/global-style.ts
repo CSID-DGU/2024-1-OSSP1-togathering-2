3f088081e443cd2a524a9f15958b48229838428e
DGUHJH <ghkdwogud852@gmail.com>
2024-03-31 12:38:27 +0900

    style : global style 추가

---
+++ frontend/src/styles/global-style.ts
@@ -0,0 +1,11 @@
+import { css } from 'styled-components'
+
+export const globalStyle = css`
+  @import url('https://cdn.jsdelivr.net/gh/wanteddev/wanted-sans@v1.0.2/packages/wanted-sans/fonts/webfonts/variable/split/WantedSansVariable.min.css');
+
+  * {
+    font-family: 'Wanted Sans Variable', 'Wanted Sans', -apple-system, BlinkMacSystemFont, system-ui, 'Segoe UI',
+      'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+      sans-serif;
+  }
+`
