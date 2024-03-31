4dd36ac1c72aaab89860fc42806adbf632787003
DGUHJH <ghkdwogud852@gmail.com>
2024-03-31 12:58:37 +0900

    feat : MainPage create menu buttons

--- frontend/src/pages/Main/MainPage.tsx
+++ frontend/src/pages/Main/MainPage.tsx
@@ -1,10 +1,54 @@
 import { FC } from 'react'
-import { Root } from './styled'
+import { useNavigate } from 'react-router-dom'
+import {
+  ContentButton,
+  ContentButtonContainer,
+  ContentContainer,
+  Root,
+  SubtitleTypo,
+  TitleContainer,
+  TitleTypo,
+} from './styled'
 
 type MainPageProps = {
   className?: string
 }
 
 export const MainPage: FC<MainPageProps> = ({ className }) => {
-  return <Root className={className}>MainPage</Root>
+  const navigate = useNavigate()
+
+  const onClickButtonPloggingCourseList = () => {
+    navigate('/plogging/course/list')
+    return
+  }
+
+  const onClickButtonPloggingGroupCreate = () => {
+    return
+  }
+
+  const onClickButtonPloggingGroupJoin = () => {
+    return
+  }
+
+  return (
+    <Root className={className}>
+      <TitleContainer>
+        <TitleTypo>같이줍깅</TitleTypo>
+        <SubtitleTypo>모두가 함께하는 플로깅</SubtitleTypo>
+      </TitleContainer>
+      <ContentContainer>
+        <ContentButtonContainer>
+          <ContentButton type={'dashed'} size={'large'} onClick={onClickButtonPloggingCourseList}>
+            플로깅 코스 추천받기
+          </ContentButton>
+          <ContentButton type={'default'} size={'large'} onClick={onClickButtonPloggingGroupCreate}>
+            플로깅 모임 만들기
+          </ContentButton>
+          <ContentButton type={'primary'} size={'large'} onClick={onClickButtonPloggingGroupJoin}>
+            플로깅 모임 참여하기
+          </ContentButton>
+        </ContentButtonContainer>
+      </ContentContainer>
+    </Root>
+  )
 }
--- frontend/src/pages/Main/styled.ts
+++ frontend/src/pages/Main/styled.ts
@@ -1,3 +1,61 @@
+import { Button, Typography } from 'antd'
 import styled from 'styled-components'
 
-export const Root = styled.div``
+export const Root = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+  box-sizing: border-box;
+`
+
+export const TitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+`
+
+export const TitleTypo = styled(Typography)`
+  &&& {
+    font-size: 28px;
+    font-weight: bold;
+    color: #111;
+  }
+`
+
+export const SubtitleTypo = styled(Typography)`
+  &&& {
+    font-size: 16px;
+    font-weight: bold;
+    color: #777;
+  }
+`
+
+export const ContentContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 40px;
+`
+
+export const ContentButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
+export const ContentButton = styled(Button)`
+  width: 100%;
+`
+
+export const ContentButtonTypo = styled(Typography)`
+  &&& {
+    font-size: 16px;
+  }
+`
