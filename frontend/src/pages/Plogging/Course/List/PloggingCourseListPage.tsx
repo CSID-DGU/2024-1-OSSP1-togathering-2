575cc2ef7b8b2ece7e54d96c4bb353452da94bbb
DGUHJH <ghkdwogud852@gmail.com>
2024-03-31 13:02:34 +0900

    feat : PlogginCourseListPage create menu button

--- frontend/src/pages/Plogging/Course/List/PloggingCourseListPage.tsx
+++ frontend/src/pages/Plogging/Course/List/PloggingCourseListPage.tsx
@@ -1,10 +1,30 @@
 import { FC } from 'react'
-import { Root } from './styled'
+import { useNavigate } from 'react-router-dom'
+import { ContentButton, ContentButtonContainer, CourseContainer, Root, TitleContainer, TitleTypo } from './styled'
 
 type PloggingCourseListPageProps = {
   className?: string
 }
 
 export const PloggingCourseListPage: FC<PloggingCourseListPageProps> = ({ className }) => {
-  return <Root className={className}>PloggingCourseListPage</Root>
+  const navigate = useNavigate()
+
+  const onClickButtonPlogginCourseCreate = () => {
+    navigate('/plogging/course/create')
+    return
+  }
+
+  return (
+    <Root className={className}>
+      <TitleContainer>
+        <TitleTypo>플로깅 코스 추천받기</TitleTypo>
+      </TitleContainer>
+      <ContentButtonContainer>
+        <ContentButton type={'primary'} onClick={onClickButtonPlogginCourseCreate}>
+          플로깅 코스 만들기
+        </ContentButton>
+      </ContentButtonContainer>
+      <CourseContainer>플로깅 코스 리스트</CourseContainer>
+    </Root>
+  )
 }
--- frontend/src/pages/Plogging/Course/List/styled.ts
+++ frontend/src/pages/Plogging/Course/List/styled.ts
@@ -1,3 +1,46 @@
+import { Button, Typography } from 'antd'
 import styled from 'styled-components'
 
-export const Root = styled.div``
+export const Root = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px;
+  box-sizing: border-box;
+`
+
+export const TitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+`
+
+export const TitleTypo = styled(Typography)`
+  &&& {
+    font-size: 28px;
+    font-weight: bold;
+    color: #111;
+  }
+`
+
+export const ContentButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
+export const ContentButton = styled(Button)`
+  width: 100%;
+`
+
+export const CourseContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`
