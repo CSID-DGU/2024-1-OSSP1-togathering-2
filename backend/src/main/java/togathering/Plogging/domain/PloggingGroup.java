723f4d46c14bad277edd4e515eb439a2127a3170
Eunji <129590633+bianbbc87@users.noreply.github.com>
2024-04-14 15:26:28 +0900

    refactor : plogging group domain에 date 추가

--- backend/src/main/java/togathering/Plogging/domain/PloggingGroup.java
+++ backend/src/main/java/togathering/Plogging/domain/PloggingGroup.java
@@ -3,12 +3,14 @@ package togathering.Plogging.domain;
 import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
 import togathering.Plogging.domain.common.BaseEntity;
 import togathering.Plogging.domain.enums.PloggingGroupStatus;
 import togathering.Plogging.domain.mapping.PloggingGroupPicture;
 import togathering.Plogging.domain.mapping.PloggingGroupReview;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -30,6 +32,9 @@ public class PloggingGroup extends BaseEntity {
 
     @Column(nullable = false)
     private String address;
+
+    @Column(nullable = false)
+    private LocalDateTime date_of_progress;
 
     @Enumerated(EnumType.STRING)
     @Column(columnDefinition = "VARCHAR(15) DEFAULT 'BEFORE'")
