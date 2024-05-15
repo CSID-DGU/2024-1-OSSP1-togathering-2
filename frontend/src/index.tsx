c7c2e1689c8b29a98d13c91e9f05894ff5a37919
DGUHJH <ghkdwogud852@gmail.com>
2024-05-15 15:53:45 +0900

    feat : PloggingSoloCreatePage 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import 'global.css'
 import { MainPage } from 'pages/Main'
 import { PloggingCourseCreatePage } from 'pages/Plogging/Course/Create'
 import { PloggingCourseListPage } from 'pages/Plogging/Course/List'
+import { PloggingSoloCreatePage } from 'pages/Plogging/Solo/Create'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -17,6 +18,7 @@ root.render(
         <Routes>
           <Route path="/plogging/course/list" element={<PloggingCourseListPage />} />
           <Route path="/plogging/course/create" element={<PloggingCourseCreatePage />} />
+          <Route path="/plogging/solo/create" element={<PloggingSoloCreatePage />} />
           <Route path="/" element={<MainPage />} />
         </Routes>
       </CommonContainer>
