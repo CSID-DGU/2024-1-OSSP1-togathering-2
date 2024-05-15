72ed1a9829f6c7e287330608b3c1ba2b7dc9655b
DGUHJH <ghkdwogud852@gmail.com>
2024-05-15 21:41:03 +0900

    feat : PloggingMeetingViewer 추가

---
+++ frontend/src/pages/Plogging/components/PloggingMeetingViewer/PloggingMeetingViewer.tsx
@@ -0,0 +1,98 @@
+import { useBooleanState } from 'hooks/useBooleanState'
+import { FC } from 'react'
+import { Map, MapMarker, Polyline } from 'react-kakao-maps-sdk'
+import { MeetingItemType } from 'types/meeting'
+import {
+  KakaoMapContainer,
+  KakaoMapMenuContainer,
+  KakaoMapMenuSwitch,
+  MapMarkerContentContainer,
+  MapMarkerContentTypo,
+  MenuContainer,
+  NameTypo,
+  Root,
+  SelectButton,
+  SelectButtonTypo,
+} from './styled'
+
+type PloggingMeetingViewerProps = {
+  className?: string
+  meetingItem: MeetingItemType
+  isDetail?: boolean
+  onSelect?: () => void
+}
+
+export const PloggingMeetingViewer: FC<PloggingMeetingViewerProps> = ({
+  className,
+  meetingItem,
+  onSelect,
+  isDetail = false,
+}) => {
+  const { state: courseCoordinateFlagActivate, toggleState: toggleCourseCoordinateFlagActivate } =
+    useBooleanState(false)
+
+  if (!meetingItem) {
+    return <div>올바르지 않은 접근입니다.</div>
+  }
+
+  const cameraInitialCoordinate = meetingItem.courseItem.coordinateList[0]
+  const courseCoordinateList = meetingItem.courseItem.coordinateList
+  const meetingName = meetingItem.name
+  const maxCount = meetingItem.maxCount
+
+  return (
+    <Root className={className}>
+      <KakaoMapContainer isDetail={isDetail}>
+        <Map
+          center={cameraInitialCoordinate}
+          style={{
+            width: '100%',
+            height: isDetail ? '400px' : '250px',
+          }}
+          level={4}
+        >
+          <Polyline
+            path={courseCoordinateList}
+            strokeWeight={5}
+            strokeColor={'#50bcdf'}
+            strokeOpacity={0.7}
+            strokeStyle={'solid'}
+          />
+          {courseCoordinateList.map(
+            (value, index) =>
+              (index === 0 || index === courseCoordinateList.length - 1 || courseCoordinateFlagActivate) && (
+                <MapMarker position={value} key={`flag_list_${index}`} clickable={true}>
+                  {courseCoordinateFlagActivate && (
+                    <MapMarkerContentContainer>
+                      {index === 0 && <MapMarkerContentTypo>출발지점</MapMarkerContentTypo>}
+                      {index !== 0 && index !== courseCoordinateList.length - 1 && (
+                        <MapMarkerContentTypo>경유지 {index + 1}</MapMarkerContentTypo>
+                      )}
+                      {index !== 0 && index === courseCoordinateList.length - 1 && (
+                        <MapMarkerContentTypo>종착지점</MapMarkerContentTypo>
+                      )}
+                    </MapMarkerContentContainer>
+                  )}
+                </MapMarker>
+              )
+          )}
+        </Map>
+        <KakaoMapMenuContainer>
+          <KakaoMapMenuSwitch value={courseCoordinateFlagActivate} onClick={toggleCourseCoordinateFlagActivate} />
+        </KakaoMapMenuContainer>
+      </KakaoMapContainer>
+      {meetingName && !isDetail && (
+        <MenuContainer>
+          <NameTypo>
+            모임 이름 : {meetingName} <br /> 최대 인원: {maxCount}
+          </NameTypo>
+          {onSelect && (
+            <SelectButton type={'primary'} onClick={onSelect}>
+              <SelectButtonTypo>모임 선택</SelectButtonTypo>
+            </SelectButton>
+          )}
+        </MenuContainer>
+      )}
+    </Root>
+  )
+}
---
+++ frontend/src/pages/Plogging/components/PloggingMeetingViewer/styled.ts
@@ -0,0 +1,67 @@
+import { Button, Switch, Typography } from 'antd'
+import styled from 'styled-components'
+import { lightTheme } from 'styles/theme'
+
+export const Root = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const KakaoMapContainer = styled.div<{ isDetail: boolean }>`
+  width: 100%;
+  height: ${(props) => (props.isDetail ? '300px' : '200px')};
+  border: 1px #d9d9d9 solid;
+  border-radius: ${(props) => (props.isDetail ? '4px' : '4px 4px 0 0')};
+  box-sizing: border-box;
+  overflow: hidden;
+  position: relative;
+`
+
+export const KakaoMapMenuContainer = styled.div`
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  z-index: 2;
+`
+
+export const MapMarkerContentContainer = styled.div`
+  width: 150px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const MapMarkerContentTypo = styled(Typography)`
+  &&& {
+    display: inline;
+    font-size: 12px;
+    color: #999;
+  }
+`
+
+export const KakaoMapMenuSwitch = styled(Switch)``
+export const MenuContainer = styled.div`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px #d9d9d9 solid;
+  border-top: 0;
+  padding: 0 10px;
+`
+
+export const NameTypo = styled(Typography)`
+  font-size: 14px;
+  color: #777;
+`
+
+export const SelectButton = styled(Button)`
+  background: ${lightTheme.colors.primary['600']};
+`
+export const SelectButtonTypo = styled(Typography)`
+  color: ${lightTheme.colors.base['50']};
+`
