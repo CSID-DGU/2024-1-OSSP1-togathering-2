54696af36101076d26fd29da8f9873c804cb5d10
DGUHJH <ghkdwogud852@gmail.com>
2024-05-15 20:35:40 +0900

    refactor : PloggingSoloCoursePage folder structure

--- frontend/src/pages/Plogging/Meeting/List/styled.ts
+++ frontend/src/pages/Plogging/Meeting/List/styled.ts
@@ -1,9 +1,25 @@
+import { Typography } from 'antd'
 import styled from 'styled-components'
+import { lightTheme } from 'styles/theme'
 
 export const Root = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
   gap: 10px;
-  padding-bottom: 40px;
+  padding: 20px;
+  padding-top: 60px;
+`
+
+export const SubtitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`
+
+export const SubtitleTypo = styled(Typography)`
+  font-size: 18px;
+  font-weight: 500;
+  color: ${lightTheme.colors.base['700']};
 `
