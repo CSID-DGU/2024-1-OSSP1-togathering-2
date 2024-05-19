4088841157d1e48093c8a0e01c91c77042b1c2ea
taehwan <ltw0527@naver.com>
2024-05-19 19:56:13 +0900

    feat : cors설정과 securityconfig 수정 #5

--- backend/src/main/java/togathering/Plogging/config/SecurityConfig.java
+++ backend/src/main/java/togathering/Plogging/config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package togathering.Plogging.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -14,11 +12,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import togathering.Plogging.jwt.CustomLogoutFilter;
 import togathering.Plogging.jwt.JWTFilter;
 import togathering.Plogging.jwt.JWTUtil;
 import togathering.Plogging.jwt.LoginFilter;
 import togathering.Plogging.oauth2.CustomSuccessHandler;
+import togathering.Plogging.repository.RefreshRepository;
 import togathering.Plogging.service.CustomOAuth2UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -27,8 +33,10 @@ public class SecurityConfig {
 
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JWTUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomSuccessHandler customSuccessHandler;
+
 
     //AuthenticationManager Bean 등록
     @Bean
@@ -51,7 +59,26 @@ public class SecurityConfig {
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
 
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                }));
         //csrf disable
         http.csrf().disable();
 
@@ -75,6 +102,7 @@ public class SecurityConfig {
                 .antMatchers("/swagger-resources/**").permitAll() // swagger
                 .antMatchers("/user/login", "/", "/user/join").permitAll()
                 .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/reissue").permitAll()
                 .antMatchers("/").permitAll()   //이부분 조심
                 .anyRequest().authenticated());
 
@@ -82,8 +110,9 @@ public class SecurityConfig {
                 .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
 
         http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
-
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, refreshRepository), UsernamePasswordAuthenticationFilter.class);
+        http
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshRepository), LogoutFilter.class);
         //세션 설정
         http
                 .sessionManagement((session) -> session
