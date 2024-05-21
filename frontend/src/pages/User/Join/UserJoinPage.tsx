e8e9685d4358afc4a90d27b567f0bbea51e31d4a
Yujin031009 <115140447+Yujin031009@users.noreply.github.com>
2024-05-21 16:13:14 +0900

    feat : UserJoinPage 수정

--- frontend/src/pages/User/Join/UserJoinPage.tsx
+++ frontend/src/pages/User/Join/UserJoinPage.tsx
@@ -1,9 +1,31 @@
-import { Root } from "./styled"
+import { Root } from './styled'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Header } from 'components/Header'
+import { TabBar } from 'components/TabBar'
 
 export const UserJoinPage = () => {
-    return (
-        <Root>
-            <h1>Join page</h1>
-        </Root>
-    )
+  return (
+    <Root>
+      <Header showLogo={true} />
+      <div className="MainFrame">
+        <p className="PageTitle">가입하기</p>
+        <hr className="Line" />
+        <form className="JoinBox">
+          이름
+          <input className="InputBox" name="UserName" type="text" />
+          아이디
+          <input className="InputBox" name="UserID" type="text" />
+          비밀번호
+          <input className="InputBox" name="UserPW" type="text" />
+          이메일
+          <input className="InputBox" name="UserEmail" type="text" />
+          <Link className="JoinLink" to="/">
+            <input className="JoinButton" type="submit" value="회원가입" />
+          </Link>
+        </form>
+      </div>
+      <TabBar />
+    </Root>
+  )
 }
