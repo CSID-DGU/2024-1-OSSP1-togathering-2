1e4b99c21738e0ca7ad902db8a26b971ac62f8cd
okseonwoo <dhrtjsdn0086@naver.com>
2024-05-25 22:27:22 +0900

    feat : 코스 추천 리스트, 태그 수정, 코스 수정시 IsHidden을 통해 수정하기 구현 쿼리 서비스 선언

---
+++ backend/src/main/java/togathering/Plogging/service/PCsQueryService.java
@@ -0,0 +1,24 @@
+package togathering.Plogging.service;
+
+import org.springframework.web.multipart.MultipartFile;
+import togathering.Plogging.app.dto.PloggingCourseDTO;
+import togathering.Plogging.app.dto.PloggingGroupReviewDTO;
+import togathering.Plogging.domain.PloggingCourse;
+import togathering.Plogging.domain.mapping.PloggingGroupReview;
+
+import java.util.List;
+
+public interface PCsQueryService {
+    List<PloggingCourseDTO.GetPloggingCourseInfoDTO> getCoursesList();
+    PloggingCourseDTO.ResponsePloggingCourseDTO createPloggingCourse(PloggingCourseDTO.RequestPloggingCourseDTO request);
+    PloggingGroupReviewDTO.ResponsePloggingGroupReviewDTO createPloggingGroupReivew(PloggingGroupReviewDTO.RequestPloggingGroupReviewDTO request);
+    PloggingGroupReview getReview(Long review_id);
+
+    PloggingCourseDTO.ResponseModifyCourseTagDTO modifyCourseTag(PloggingCourseDTO.RequestModifyCourseTagDTO request, Long id);
+
+    PloggingCourseDTO.GetPloggingCourseInfoDTO getCourseInfo(Long course_id);
+
+    PloggingCourseDTO.ResponsePloggingCourseDTO modifyCourse(PloggingCourseDTO.RequestPloggingCourseDTO request, Long id);
+    List<PloggingCourseDTO.ResponsePloggingCourseDTO> getRecommendCourseList(String tag);
+    List<PloggingCourseDTO.ResponsePloggingCourseDTO> getCourseListSearchBy(String word);
+}
