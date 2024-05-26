6d82a51790ff5967fa356262f7d72dea86592eb9
taehwan <ltw0527@naver.com>
2024-05-26 19:30:58 +0900

    feat: API Response와 statusResponse class 추가 및 여러 에러 코드 추가

--- backend/src/main/java/togathering/Plogging/apiPayload/code/status/ErrorStatus.java
+++ backend/src/main/java/togathering/Plogging/apiPayload/code/status/ErrorStatus.java
@@ -26,8 +26,18 @@ public enum ErrorStatus implements BaseErrorCode {
 
     ALREADY_JOIN_GROUP(HttpStatus.BAD_REQUEST, 40901, "이미 참가 중인 그룹입니다."),
     ALREADY_EXIT_GROUP(HttpStatus.BAD_REQUEST, 40902, "이미 탈퇴한 그룹입니다."),
-    ADMIN_CANNOT_EXIT(HttpStatus.BAD_REQUEST, 40903, "그룹장은 탈퇴할 수 없습니다.");
-
+    ADMIN_CANNOT_EXIT(HttpStatus.BAD_REQUEST, 40903, "그룹장은 탈퇴할 수 없습니다."),
+    // 회원가입 실패
+    REGISTER_ALREADY_USER_EXIST(HttpStatus.BAD_REQUEST, 41001, "이미 있는 아이디(이메일)입니다."),
+    REGISTER_NICKNAME_EMPTY(HttpStatus.BAD_REQUEST, 41002, "닉네임을 입력해주세요."),
+    REGISTER_NICKNAME_PUNCT(HttpStatus.BAD_REQUEST, 41003, "닉네임에는 특수문자가 들어갈 수 없습니다."),
+    REGISTER_NICKNAME_TOO_LONG(HttpStatus.BAD_REQUEST, 41004, "닉네임은 최대 50자입니다."),
+    REGISTER_EMAIL_INVALID(HttpStatus.BAD_REQUEST, 41005, "이메일 형식이 아닙니다."),
+    REGISTER_EMAIL_EMPTY(HttpStatus.BAD_REQUEST, 41006, "이메일을 입력해주세요."),
+    REGISTER_EMAIL_TOO_LONG(HttpStatus.BAD_REQUEST, 41006, "이메일은 최대 50자입니다."),
+    REGISTER_PASSWORD_INVALID(HttpStatus.BAD_REQUEST, 41007, "비밀번호 형식이 아닙니다."),
+    REGISTER_PASSWORD_EMPTY(HttpStatus.BAD_REQUEST, 41008, "비밀번호를 입력해주세요."),
+    REGISTER_PASSWORD_TOO_LONG(HttpStatus.BAD_REQUEST, 41009, "비밀번호는 최대 70자입니다.");
 
     // 여기 관련해서 하고싶은 응답들 정의해주세요..
 
