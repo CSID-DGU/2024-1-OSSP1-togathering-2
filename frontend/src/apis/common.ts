c98b0f66c84281a42ff8dcbe41af96a45fc873d1
DGUHJH <ghkdwogud852@gmail.com>
2024-05-27 22:17:03 +0900

    feat : commonAxios 추가

--- frontend/src/apis/common.ts
+++ frontend/src/apis/common.ts
@@ -1,1 +1,13 @@
+import axios from 'axios'
+
 export const OPEN_AI_API_KEY = process.env['REACT_APP_OPENAI_KEY']
+export const API_URL = 'https://togethering.store/'
+
+// 공통 설정을 위한 axios 인스턴스 생성
+export const commonAxios = axios.create({
+  baseURL: API_URL, // 기본 URL 설정
+  timeout: 10000, // 요청 타임아웃 설정 (10초)
+  headers: {
+    'Content-Type': 'application/json', // 기본 헤더 설정
+  },
+})
