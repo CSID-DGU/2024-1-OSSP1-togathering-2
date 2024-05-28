ee3b330187321b59b4db6cd428c9b171109e65e9
okseonwoo <dhrtjsdn0086@naver.com>
2024-05-28 17:10:17 +0900

    feat : AI 추천 리스트 불러오기 QueryServie 선언

--- backend/src/main/java/togathering/Plogging/service/PCsQueryService.java
+++ backend/src/main/java/togathering/Plogging/service/PCsQueryService.java
@@ -6,12 +6,13 @@ import togathering.Plogging.app.dto.PloggingGroupReviewDTO;
 import togathering.Plogging.domain.PloggingCourse;
 import togathering.Plogging.domain.mapping.PloggingGroupReview;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface PCsQueryService {
     List<PloggingCourseDTO.GetPloggingCourseInfoDTO> getCoursesList();
     PloggingCourseDTO.ResponsePloggingCourseDTO createPloggingCourse(PloggingCourseDTO.RequestPloggingCourseDTO request);
-    PloggingGroupReviewDTO.ResponsePloggingGroupReviewDTO createPloggingGroupReivew(PloggingGroupReviewDTO.RequestPloggingGroupReviewDTO request);
+    PloggingGroupReviewDTO.ResponsePloggingGroupReviewDTO createPloggingGroupReivew(PloggingGroupReviewDTO.RequestPloggingGroupReviewDTO request) throws IOException;
     PloggingGroupReview getReview(Long review_id);
 
     PloggingCourseDTO.ResponseModifyCourseTagDTO modifyCourseTag(PloggingCourseDTO.RequestModifyCourseTagDTO request, Long id);
@@ -19,6 +20,6 @@ public interface PCsQueryService {
     PloggingCourseDTO.GetPloggingCourseInfoDTO getCourseInfo(Long course_id);
 
     PloggingCourseDTO.ResponsePloggingCourseDTO modifyCourse(PloggingCourseDTO.RequestPloggingCourseDTO request, Long id);
-    List<PloggingCourseDTO.ResponsePloggingCourseDTO> getRecommendCourseList(String tag);
+    List<PloggingCourseDTO.ResponsePloggingCourseDTO> getRecommendCourseListByAI(PloggingCourseDTO.RequestRecommendCourseDTO dto);
     List<PloggingCourseDTO.ResponsePloggingCourseDTO> getCourseListSearchBy(String word);
 }
